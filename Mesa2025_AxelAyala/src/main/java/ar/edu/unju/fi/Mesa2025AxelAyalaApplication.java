788ff5757fda737a1acefdbaaf6de59c70a4231f
axelayala <axelcapofg@gmail.com>
2025-03-08 23:22:30 -0300

    Inicializacion del Proyecto

---
+++ Mesa2025_AxelAyala/src/main/java/ar/edu/unju/fi/Mesa2025AxelAyalaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mesa2025AxelAyalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mesa2025AxelAyalaApplication.class, args);
+	}
+
+}
